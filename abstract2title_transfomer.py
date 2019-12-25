823e23e0fa1601fbb931b1c9765409681e1094b8
Amalie Trewartha <amalie.trewartha@gmail.com>
2019-12-25 11:02:17 -0800

    Converted transfomer to adam optimizer instead of RMSprop

--- abstract2title_transfomer.py
+++ abstract2title_transfomer.py
@@ -49,13 +49,42 @@ def sparse_cross_entropy(y_true, y_pred):
   loss = tf.keras.losses.SparseCategoricalCrossentropy(
       from_logits=True, reduction='none')(y_true, y_pred)
 
-  # mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
-  # loss = tf.multiply(loss, mask)
+  mask = tf.cast(tf.not_equal(y_true, 1), tf.float32)
+  loss = tf.multiply(loss, mask)
 
   return tf.reduce_mean(loss)
 
 
-model.compile(optimizer='rmsprop', loss=sparse_cross_entropy)
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+  def __init__(self, d_model=256, warmup_steps=4000):
+    super(CustomSchedule, self).__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps**-1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+learning_rate = CustomSchedule()
+
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+
+def accuracy(y_true, y_pred):
+  y_true = tf.reshape(y_true, shape=(-1, title_maxlen))
+  return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+
+
+model.compile(optimizer=optimizer,
+              loss=sparse_cross_entropy, metrics=[accuracy])
 model.summary()
 
 
